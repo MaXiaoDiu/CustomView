6a4d08efec81c41c1e0ff855b95a66793c1d6ca1
cyy513 <chengyy@mxchip.com>
2018-04-10 11:30:04 +0800

    创建打勾动画View

---
+++ app/src/main/java/com/busu/cyy/customview/Activity/CircleCheck.java
@@ -0,0 +1,26 @@
+package com.busu.cyy.customview.Activity;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Cyy513 on 2018/4/10.
+ */
+
+public class CircleCheck extends View {
+
+
+    public CircleCheck(Context context) {
+        super(context);
+    }
+
+    public CircleCheck(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleCheck(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
